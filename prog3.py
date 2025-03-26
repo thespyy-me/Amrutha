df997e9763d30e8488dca9ea47ce4b04d0075754
thespyy-me <celestiqluna7@gmail.com>
2025-03-26 09:52:28 +0530

    Add files via upload

---
+++ prog3.py
@@ -0,0 +1,21 @@
+
+name = input("enter urname")
+email = input("enter email")
+address = input("enter address")
+phno = input("enter phone no")
+
+
+def print_info(name,email,address,phno):
+
+    print("persnal details")
+    print("name:",name)
+    print("email:",email)
+    print("address:",address)
+    print("phone number:",phno)
+
+
+
+print_info(name,email,address,phno)
+
+    
+                  
